194c8b62415b11290c4d9ba6dbfb2fdd4a7f59ec
r4f3t <parlakrafet01@gmail.com>
2022-06-21 23:01:26 +0300

    cobra added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/r4f3t/webapi
+
+go 1.16
+
+require github.com/spf13/cobra v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/r4f3t/webapi/cmd"
+
+func main() {
+	cmd.Execute()
+}
